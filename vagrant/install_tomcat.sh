5fc49cc00bfab56c49bf3cbefd36cf5568b68cf1
Ben Wallberg <wallberg@umd.edu>
2014-09-30 07:53:34 -0400

    suppress wget progress update in Vagrant provisioning scripts

--- vagrant/install_tomcat.sh
+++ vagrant/install_tomcat.sh
@@ -1,4 +1,6 @@
 #Download tomcat
-wget http://mirror.tcpdiag.net/apache/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz
+wget \
+  --no-verbose \
+  http://mirror.tcpdiag.net/apache/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz
 tar xzf apache-tomcat-7.0.55.tar.gz
 mv apache-tomcat-7.0.55 tomcat7
