d26fcdc0011c7856f0d9eaa03b533d64327a2989
Vivian Thayil <vthayil@umd.edu>
2014-10-28 16:35:02 -0400

    Minor modifications to provisioning scripts.

--- vagrant/install_jdk.sh
+++ vagrant/install_jdk.sh
@@ -1,8 +1,13 @@
 #Download and install JDK
-wget \
-  --no-verbose \
-  --no-check-certificate \
-  --no-cookies \
-  --header "Cookie: oraclelicense=accept-securebackup-cookie" \
-  http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.rpm 
-rpm -Uvh jdk-7u67-linux-x64.rpm 
+if [ ! -z `which java` ]; then
+	echo "Java already installed"
+else
+	echo "Installing Java"
+	wget \
+	  --no-verbose \
+	  --no-check-certificate \
+	  --no-cookies \
+	  --header "Cookie: oraclelicense=accept-securebackup-cookie" \
+	  http://download.oracle.com/otn-pub/java/jdk/7u67-b01/jdk-7u67-linux-x64.rpm 
+	rpm -Uvh jdk-7u67-linux-x64.rpm
+fi 
--- vagrant/install_tomcat.sh
+++ vagrant/install_tomcat.sh
@@ -1,6 +1,11 @@
 #Download tomcat
-wget \
-  --no-verbose \
-  http://mirror.tcpdiag.net/apache/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz
-tar xzf apache-tomcat-7.0.55.tar.gz
-mv apache-tomcat-7.0.55 tomcat7
+if [ -d "/home/vagrant/tomcat7" ]; then
+	echo "Tomcat already present"
+else
+	echo "Downloading tomcat"
+	wget \
+	  --no-verbose \
+	  https://archive.apache.org/dist/tomcat/tomcat-7/v7.0.55/bin/apache-tomcat-7.0.55.tar.gz
+	tar xzf apache-tomcat-7.0.55.tar.gz
+	mv apache-tomcat-7.0.55 tomcat7
+fi
