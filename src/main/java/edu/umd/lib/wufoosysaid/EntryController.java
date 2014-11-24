9d413b90f31564b8284ac25b3de88edfa20cc4d7
Rohit Arora <rohit89@umd.edu>
2014-11-24 14:25:47 -0500

    LIBITD-282. Removed commented fix.
    Removed commented code which was causing server crash in case no xsl was found.
    
    https://issues.umd.edu/browse/LIBITD-282

--- src/main/java/edu/umd/lib/wufoosysaid/EntryController.java
+++ src/main/java/edu/umd/lib/wufoosysaid/EntryController.java
@@ -242,10 +242,6 @@ public class EntryController extends HttpServlet {
       response.sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR,
           "Error occured while attempting to create requests from entry.");
       return;
-      // BAD TEMPORARY FIX, Exit if no XSL was found!
-      // (To avoid multiple do post requests by container)
-
-      // System.exit(0);
     }
   }
 }
